330082482fe4bef2ffa546f35a9366bd5c6f7677
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-07-02 11:17:55 -0700

    Use fetch, .catch and .then to make API call.
    Co-authored-by: Micheal Hansen <micheal.hansen@gmail.com>

---
+++ with-fetch.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#getAPIbutton").click(function() {
+
+    fetch(`https://dog.ceo/api/breeds/image/random`) //fetch is a shortcut; it creates a promise object that executes a GET API request on the url fed into it.
+      .then(function(responseJSON) {
+        if (!response.ok) {
+          throw Error(responseJSON.statusText); //if response is woops, pass an error status down to catch and catch will do something with it
+        }
+        return responseJSON.json(); //if response is good, pass the response down to the next .then and .then will do something with it
+      })
+      .catch(function(error) {  //only executes if response above was woops
+        return error;
+      })
+      .then(function(messageFromPrevious) { //if .catch triggered, messageFromPrevious will be the error message, if .catch did NOT trigger, messageFromPrevious will be the parsed JSON info
+        if (messageFromPrevious.message) {
+          $("#thingGoesHere").html(`It's a cute doggo <img src='${messageFromPrevious.message}' alt='Doggo McDogface'>`);
+          $("#errorHere").html('');
+        } else {
+          $("#errorHere").html(`${messageFromPrevious}`);
+          $("#thingGoesHere").html('');
+        }
+      });
+  });
+});
